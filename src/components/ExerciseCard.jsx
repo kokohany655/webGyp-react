06b740080ae56153b33d55b3b53dcbd020a6f92d
kokohany655 <https://github.com/kokohany655>
2022-10-18 08:57:27 +0200

    second change

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const ExerciseCard = ({exercise}) => {
   return (
     <Link to={`/exercise/${exercise.id}`}>
-        <Box borderRadius={'10px'} boxShadow={'0px 0px 25px  rgba(0,0,0,.7)'} _hover={{transform:'scale(1.04)'}} transition='.4s'>
+        <Box borderRadius={'10px'} boxShadow={'0px 0px 7px  rgba(0,0,0,.7)'} _hover={{transform:'scale(1.04)'}} transition='.4s'>
         <Image  mb='2' src={exercise.gifUrl} alt={exercise.name} loading='lazy' borderTopLeftRadius={'10px'} borderTopRightRadius={'10px'}/>
         <Button variant={'ghost'} >{exercise.bodyPart}</Button>
         <Button ml='4px'>{exercise.target}</Button>
